4203cb8eee4574c85ce25c57d787fdfb00b582c5
Duong Bao Duy <baoduy.duong0206@gmail.com>
2015-06-15 18:43:45 +0700

    - change script name Signed-off-by: Duong Bao Duy <baoduy.duong0206@gmail.com>

---
+++ dotenv.cmd
@@ -0,0 +1,14 @@
+:: %~dp0
+
+set PATH=C:\tools\cygwin\bin;D:\tools\go;%~dp0;%~dp0scripts;%PATH%
+set PATH=D:\tools\LLVM\bin;D:\tools\LLVM\msbuild-bin;%PATH%
+
+set HOME=D:/dbduy0206/dbd-home
+
+set NDK_ROOT=D:\tools\android-ndk
+set SDK_ROOT=D:\tools\android-sdk
+set PATH=%NDK_ROOT%;%SDK_ROOT%\platform-tools;%SDK_ROOT%\tools;%PATH%
+
+start "" /MAX "atom" --include-deprecated-apis
+:: D:\tools\eclipse-4.4\eclipse.exe --data D:\workspace-dbd
+start "" /MAX "D:\tools\eclipse-android\eclipse.exe" -data D:\workspace
