017801cac68e38dc2a090f9b91545abb8a6bbc5b
Duong Bao Duy <baoduy.duong0206@gmail.com>
2015-06-18 08:33:28 +0700

    - add python to PATH

--- dotenv.cmd
+++ dotenv.cmd
@@ -9,6 +9,10 @@ set NDK_ROOT=D:\tools\android-ndk
 set SDK_ROOT=D:\tools\android-sdk
 set PATH=%NDK_ROOT%;%SDK_ROOT%\platform-tools;%SDK_ROOT%\tools;%PATH%
 
+:: set path for python
+set PYTHON=D:\tools\python2
+set PATH=%PYTHON%;%PYTHON%\Scripts;%PATH%
+
 start "" /MAX "atom" --include-deprecated-apis
 :: D:\tools\eclipse-4.4\eclipse.exe --data D:\workspace-dbd
 start "" /MAX "D:\tools\eclipse-android\eclipse.exe" -data D:\workspace
